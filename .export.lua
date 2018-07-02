a3e801dcf1fa142046baa8173ae2aba5d74c441b
Koen Visscher <k.visscher@zefiros.eu>
2018-07-02 23:36:33 +0200

    fix build

--- .export.lua
+++ .export.lua
@@ -1,7 +1,9 @@
 project "Falcor"
     kind "StaticLib"
         
-    files "Framework/Source/API/**"
+    files {"Framework/Source/API/**",
+           "Framework/Source/Utils/Platform/OS.cpp",
+           "Framework/Source/Utils/Profiler.cpp" }
     
     filter "system:windows"
         removefiles "Framework/Source/API/Vulkan/**"
@@ -9,11 +11,21 @@ project "Falcor"
     filter "system:linux"
         removefiles "Framework/Source/API/D3D12/**"
     
+    filter ""
+    
     zpm.export [[
-        includedirs "Framework/Source/API"
+        includedirs "Framework/Source"
         cppdialect "C++11"
         
         zpm.uses {
                 "Zefiros-Software/GLM"
             }
+        
+        filter "system:windows"
+            defines "FALCOR_D3D12"
+        
+        filter "system:linux"
+            defines "FALCOR_VK"
+        
+        filter ""
     ]]
